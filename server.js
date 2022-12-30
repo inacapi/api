3fa9b1b2b755a28b60b405a483f699f6944f76af
Gabriel Barrientos <gabri12.rubik@gmail.com>
2022-12-30 20:48:59 -0300

    Ruta para seccion de forma simple

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import axios from 'axios'
 import { obtener_token } from './utils.js'
 
 const app = express()
@@ -10,20 +11,56 @@ app.listen(port, () => {
 })
 
 app.post('/obtener_token', async (req, res) => {
-    if (!req.body.nombre) {
-        res.status(400).send('Error no se encuentra el nombre de usuario')
-        return
-    }
+    if (!req.body.nombre)
+        return res.status(400).send('Error no se encuentra el nombre de usuario')
 
-    if (!req.body.contraseña) {
-        res.status(400).send('Error no escribio su contraseña no puede iniciar la sesión')
-        return
-    }
+    if (!req.body.contraseña)
+        return res.status(400).send('Error no escribio su contraseña no puede iniciar la sesión')
 
     const resultado = await obtener_token(req.body.nombre, req.body.contraseña)
 
-    if (resultado.error) {
+    if (resultado.error)
         res.status(400)
-    }
+
     res.json(resultado)
+})
+
+app.post('/seccion', async (req, res) => {
+    if (!req.headers.authorization)
+        return res.status(400).json({ error: 'No se encuentra el token' })
+
+    if (!req.body.matricula)
+        return res.status(400).json({ error: 'No se encuentra la matricula' })
+
+    if (!req.body.periodo)
+        return res.status(400).json({ error: 'No se encuentra el periodo' })
+
+    if (!req.body.seccion)
+        return res.status(400).json({ error: 'No se encuentra la seccion' })
+
+    try {
+        const respuesta = await axios.request({
+            method: 'POST',
+            url: 'https://siga.inacap.cl/Inacap.Siga.ResumenAcademico/api/seccion',
+            headers: {
+                Cookie: `HTPSESIONIC=${req.headers.authorization};`
+            },
+            data: {
+                matrNcorr: `${req.body.matricula}`,
+                seccCcod: `${req.body.seccion}`,
+                periCcod: `${req.body.periodo}`,
+                ssecNcorr: '0',
+                carrCcod: '0'
+            }
+        })
+        res.json(respuesta.data)
+    } catch (e) {
+        if (e.message.includes('code 401')) {
+            res.status(401).json({ error: 'El token venció' })
+        } else if (e.response === undefined) {
+            res.status(500).json({ error: 'Inacap no responde' })
+        } else {
+            res.json({})
+        }
+    }
 })
