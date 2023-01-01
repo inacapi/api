4462f030a2d9948517c9aa06eac9b9b8c5b3610c
Gabriel Barrientos <gabri12.rubik@gmail.com>
2023-01-01 17:16:21 -0300

    Dependencia en cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import axios from 'axios'
+import cors from 'cors'
 import { obtener_token } from './utils.js'
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 const port = process.env.PORT || 3000
