69ab7d7df1a117166dfe43359d86a3271be88c49
Gabriel Barrientos <gabri12.rubik@gmail.com>
2023-01-03 13:27:59 -0300

    Reemplazar puppeteer y node-libcurl por node-fetch-cookies

--- utils.js
+++ utils.js
@@ -1,48 +1,46 @@
-import { curly } from 'node-libcurl'
-import querystring from 'querystring'
+import { fetch, CookieJar } from 'node-fetch-cookies'
 import { parse } from 'node-html-parser'
-import fs from 'fs'
 
 const adfs = 'https://adfs.inacap.cl/adfs/ls/?wtrealm=https://siga.inacap.cl/sts/&wa=wsignin1.0&wreply=https://siga.inacap.cl/sts/&wctx=https%3a%2f%2fadfs.inacap.cl%2fadfs%2fls%2f%3fwreply%3dhttps%3a%2f%2fwww.inacap.cl%2ftportalvp%2fintranet-alumno%26wtrealm%3dhttps%3a%2f%2fwww.inacap.cl%2f'
 const sts = 'https://siga.inacap.cl/sts/'
 
 export const obtener_token = async (nombre, contraseña) => {
-    const { data } = await curly.post(adfs, {
-        verbose: true,
-
-        cookieJar: 'cookies.txt',
-        followLocation: true,
-        postFields: querystring.stringify({
+    const cookieJar = new CookieJar()
+    const respuesta = await fetch(cookieJar, adfs, {
+        method: 'POST',
+        body: new URLSearchParams({
             'UserName': nombre,
             'Password': contraseña,
             'AuthMethod': 'FormsAuthentication'
         })
     })
 
+    const data = await respuesta.text()
     if (data && !data.includes('Working...')) {
         return { error: 'Usuario o contraseña incorrectos', token: '' }
     }
 
-    try {
-        const document = parse(data)
-        const wresult = document.querySelector('input[name="wresult"]').getAttribute('value')
-        await curly.post(sts, {
-            cookieJar: 'cookies.txt',
-            followLocation: true,
-            referer: 'https://adfs.inacap.cl/',
-            postFields: querystring.stringify({
-                'wresult': wresult,
-                'wa': 'wsignin1.0',
-                'wctx': 'https://adfs.inacap.cl/adfs/ls/?wreply=https://www.inacap.cl/tportalvp/intranet-alumno&amp;wtrealm=https://www.inacap.cl/'
-            })
-        })
-        const cookies = fs.readFileSync('cookies.txt', 'utf-8')
-        const pos = cookies.indexOf('HTPSESIONIC') + 12
-        if (pos === 11) {
-            throw Error
-        }
-        return { error: '', token: cookies.slice(pos, pos + 64) }
-    } catch (e) {
-        return { error: 'Fallo al leer respuesta', token: '' }
+    const document = parse(data)
+    const wresult = document.querySelector('input[name="wresult"]')
+    if (!wresult) {
+        return { error: 'No se pudo obtener el token', token: '' }
     }
+
+    await fetch(cookieJar, sts, {
+        method: 'POST',
+        headers: { 'Referer': 'https://adfs.inacap.cl/' },
+        body: new URLSearchParams({
+            'wresult': wresult.getAttribute('value'),
+            'wa': 'wsignin1.0',
+            'wctx': 'https://adfs.inacap.cl/adfs/ls/?wreply=https://www.inacap.cl/tportalvp/intranet-alumno&amp;wtrealm=https://www.inacap.cl/'
+        })
+    })
+
+    for (const cookie of cookieJar.cookiesAll()) {
+        if (cookie.name === 'HTPSESIONIC') {
+            return { error: '', token: cookie.value }
+        }
+    }
+
+    return { error: 'No se pudo obtener el token', token: '' }
 }
